56be2740ae171c9e362494bb1627c6f7c0f62411
Redstone Zhao <13740080@qq.com>
2021-12-16 10:29:48 +0800

    修正ui配置文件

--- nodejs/src/config.js
+++ nodejs/src/config.js
@@ -5,6 +5,5 @@ module.exports = Object.freeze({
     root: "https://club.sgate.sa",
     wireRoot: "https://club.sgate.sa/wire-transfer/",
     hostedRoot: "https://club.sgate.sa/mastercard-hosted/",
-    hostedRoot: "http://opay.xiongfei.me/mastercard-hosted/",
   },
 });
